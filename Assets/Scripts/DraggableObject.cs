d67c3dc21d5bbac2db94dbd96bfbe5fbcf01118e
Boyan Botev <boyanbotev@gmail.com>
2024-11-03 18:55:05 +0200

    feat: snapping into place

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class DraggableObject : MonoBehaviour
 {
+    [SerializeField] public Transform target;
     private bool isDragging = false;
+    private bool isSnapped = false;
+    private float minSnapDistance = 1f;
     public void OnClick() {
-        Debug.Log("Clicked on " + gameObject.name);
-
+        if (isSnapped) return;
         isDragging = true;
     }
 
     public void OnDrag()
     {
-        if (!isDragging) return;
+        if (!isDragging || isSnapped) return;
 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
@@ -23,5 +25,10 @@ public class DraggableObject : MonoBehaviour
     public void OnRelease()
     {
         isDragging = false;
+        if (Vector3.Distance(transform.position, target.position) < minSnapDistance)
+        {
+            transform.position = target.position;
+            isSnapped = true;
+        }
     }
 }
