e86f54309e2348e184fe795e4bfa5e0563dfd325
Boyan Botev <boyanbotev@gmail.com>
2024-11-06 21:51:51 +0200

    fix: add unversioned files

---
+++ Assets/Scripts/cards/Card.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Card : VisualElement
+{
+    public static event Action<string> onFlip;
+    public string letter;
+    public bool isShown = false;
+    public bool isRemoved = false;
+
+    public Card(string letter)
+    {
+        AddToClassList("card");
+        RegisterCallback<MouseDownEvent>(evt => Flip());
+        this.letter = letter;
+
+        var label = new Label(letter);
+        Add(label);
+    }
+
+    public void Flip()
+    {
+        if (isRemoved) return;
+
+        if (isShown)
+        {
+            isShown = false;
+            RemoveFromClassList("shown");
+        }
+        else
+        {
+            isShown = true;
+            AddToClassList("shown");
+            onFlip?.Invoke(letter);
+        }
+    }
+
+    public void Remove()
+    {
+        AddToClassList("removed");
+        isRemoved = true;
+    }
+
+}
---
+++ Assets/Scripts/cards/CardsGameManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System;
+
+public class CardsGameManager : MonoBehaviour
+{
+    [SerializeField] private string[] letters;
+    UIDocument uIDoc;
+    VisualElement root;
+    VisualElement cardsContainer;
+    private int letterIndex = 0;
+    private int rowCount = 2;
+    private int cardPerRow = 4;
+    private List<Card> cards = new List<Card>();
+    private int cardsShown;
+
+    private void Awake()
+    {
+        uIDoc = FindObjectOfType<UIDocument>();
+        root = uIDoc.rootVisualElement;
+        cardsContainer = root.Q<VisualElement>(className: "cards");
+    }
+
+    private void OnEnable()
+    {
+        Card.onFlip += OnCardFlip;
+    }
+
+    private void OnDisable()
+    {
+        Card.onFlip -= OnCardFlip;
+    }
+
+    private void Start()
+    {
+        BuildCards();
+    }
+
+    void BuildCards()
+    {
+        for (int i = 0; i < rowCount; i++)
+        {
+            BuildRow();
+        }
+    }
+
+    void OnCardFlip(string letter)
+    {
+        var isWinning = false;
+        foreach (var card in cards)
+        {
+            if (card.letter.ToLower() == letter.ToLower() && card.isShown && card.letter != letter)
+            {
+               RemoveWinningCards(letter);
+               isWinning = true;
+               cardsShown = 0;
+            }
+        }
+
+        if (!isWinning)
+        {
+            cardsShown++;
+
+            if (cardsShown == 2)
+            {
+                foreach (var card in cards)
+                {
+                    if (card.isShown)
+                    {
+                        StartCoroutine(FalseCardRoutine(card));
+                    }
+                }
+                cardsShown = 0;
+            }
+        }
+
+    }
+
+    private void RemoveWinningCards(string letter)
+    {
+        foreach (var card in cards)
+        {
+            if (card.letter.ToLower() == letter.ToLower())
+            {
+                card.Remove();
+            }
+        }
+    }
+
+    VisualElement BuildRow()
+    {
+        var row = new VisualElement();
+        row.AddToClassList("cards-row");
+        cardsContainer.Add(row);
+
+        for (int j = 0; j < cardPerRow; j++)
+        {
+            var card = new Card(letters[letterIndex]);
+            letterIndex++;
+            row.Add(card);
+            cards.Add(card);
+        }
+        return row;
+    }
+
+    IEnumerator FalseCardRoutine(Card card)
+    {
+        yield return new WaitForSeconds(0.4f);
+        card.Flip();
+    }
+}
